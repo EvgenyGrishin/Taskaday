5329bff69b5e4630bd447cf42797ac5de863bfe0
EvgenyGrishin <egrishin@gmail.com>
2018-02-12 23:27:32 +0300

    1

--- app/src/main/java/com/egrishin/task_a_day/MainActivity.java
+++ app/src/main/java/com/egrishin/task_a_day/MainActivity.java
@@ -3,10 +3,13 @@ package com.egrishin.task_a_day;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,15 +25,22 @@ public class MainActivity extends AppCompatActivity {
     public void plusTextField(View view) {
 
         if (numberOfLines > 0) {
-            LinearLayout ll = (LinearLayout) findViewById(R.id.ll1);
+            LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayout1);
+
             // add edittext
             EditText et = new EditText(this);
             LinearLayout.LayoutParams p = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
             et.setLayoutParams(p);
-            et.setText("Text1");
+            et.setText("text" + numberOfLines);
             et.setId(numberOfLines - 1);
             ll.addView(et);
             numberOfLines--;
+
+            //button counter
+
+            Button secButton = (Button)findViewById(R.id.secondary_button);
+            secButton.setText("" + numberOfLines);
+
         }
 
         else {
